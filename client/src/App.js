4590a06702bb86e86f6aa34852df453d82f1b452
zacharyarney <zachary.arney@gmail.com>
2019-09-05 22:43:30 -0700

    makes App class component, mount axios calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import UserCard from './components/UserCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    userData: {},
+    followers: [],
+  };
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/zacharyarney').then((res) => {
+      this.setState({ userData: res.data });
+      // console.log(res.data)
+    });
+    axios
+      .get('https://api.github.com/users/zacharyarney/followers')
+      .then((res) => {
+        this.setState({ followers: res.data });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <UserCard
+          userData={this.state.userData}
+          followers={this.state.followers}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
